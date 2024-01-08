aeeffb19efcfa63945ad9185a74c5f8418e8f1c5
maciej.lyszczarz <maciej.lyszczarz@jamf.com>
2024-01-08 07:57:13 +0100

    fix test with fakeasync

--- src/app/shared/data-access/store.test.ts
+++ src/app/shared/data-access/store.test.ts
@@ -1,4 +1,4 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, fakeAsync, tick } from '@angular/core/testing';
 import {
   HttpClientTestingModule,
   HttpTestingController,
@@ -21,7 +21,7 @@ describe('AdviceStore', () => {
     store = TestBed.inject(AdviceStore);
   });
 
-  it('should get random advice successfully', () => {
+  it('should get random advice successfully', fakeAsync(() => {
     const mockSlip: Slip = {
       slip: { id: 1, advice: 'Test advice' },
     };
@@ -33,12 +33,13 @@ describe('AdviceStore', () => {
     expect(req.request.method).toEqual('GET');
 
     req.flush(mockSlip);
-    httpController.verify();
+    tick(1000);
 
     expect(store.id()).toEqual(mockSlip.slip.id);
     expect(store.advice()).toEqual(mockSlip.slip.advice);
     expect(store.quotedAdvice()).toEqual(`"${mockSlip.slip.advice}"`);
-  });
+    httpController.verify();
+  }));
 
   it('should set an error msg when error occurs', () => {
     const emsg = 'deliberate 404 error';
